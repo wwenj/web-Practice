8b48892cb961f586baf333bbf1dfa2dfc02dd1fa
wwenj <wang8151186051>
2017-04-25 18:14:49 +0800

    简历完成

--- jq插件个人简历/js/index.js
+++ jq插件个人简历/js/index.js
@@ -5,10 +5,21 @@ $(function(){
 		'navigationColor':'red',
 		'loopBottom':true,
 		onLeave:function(index,nextIndex,direction ){
-			$(".aa").eq(index-1).css('background','#6EE95E')
+			//添加css3动画实现页面翻转背景色渐变
+			$(".aa").eq(index-1).css({animation:'myfirst 0.6s'});
+			//$(".aa").eq(index-1).animate({background:'#6EE95E'})
 		},
 		afterLoad:function(anchorLink,index){
-			$(".aa").siblings().css('background','#1bbc9b')
+			//翻转过后颜色变回
+			$(".aa").siblings().css({background:'#22C3AA',animation:'myfirst1 0.5s'})
 		},
+	})
+	
+	$("li").mouseenter(function(){
+		/*$(".navTitle").eq($(this).index()).css('opacity','1').siblings().css('opacity','0');*/
+		$("i").eq($(this).index()).fadeIn('normal');
+	})
+	$("li").mouseleave(function(){
+		$("i").fadeOut(300);
 	})
 });
